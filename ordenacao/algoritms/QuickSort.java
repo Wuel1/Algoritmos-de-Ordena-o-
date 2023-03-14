8a7369602957f61337ed0893798ed14c33dba021
Wandson Emanuel <wandson7emanuel@hotmail.com>
2023-03-13 23:13:45 -0300

    Fazendo o QuickSort

--- ordenacao/algoritms/QuickSort.java
+++ ordenacao/algoritms/QuickSort.java
@@ -7,7 +7,38 @@ public class QuickSort implements Sorter {
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+        int começo = sorted[0];
+        int fim = sorted[sorted.length-1];
+        if(começo < fim){
+            int[] PRIpivo = parte(sorted, começo, fim);
+            sort(PRIpivo);
+            return PRIpivo;
+            }
+            return sorted;
+        }
+    
+    
+    public int[] parte(int[] sorted, int menores, int maiores){        
+        int pivo = sorted[0];
+        int i = 0;
+        int j = sorted.length - 1;
+        while(i<=j){
+            if(sorted[i]<= pivo){
+                i = i+1;
+            }
+            else if(pivo < sorted[j]){
+                j = j-1;
+            }
+            else{
+                int controle = sorted[i];
+                sorted[i] = sorted[j];
+                sorted[j] = controle;
+                i = i+1;
+                j = j-1;
+            }
+        }        
+        sorted[i] = sorted[j];
+        sorted[j] = pivo;
         return sorted;
     }
 
