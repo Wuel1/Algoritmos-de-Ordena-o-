c5725e7d44f377dcbeea4a0b71e58aec099aebdf
Wandson Emanuel <wandson7emanuel@hotmail.com>
2023-03-16 19:55:35 -0300

    Finalizado

--- ordenacao/algoritms/MergeSort.java
+++ ordenacao/algoritms/MergeSort.java
@@ -1,51 +1,52 @@
 package ordenacao.algoritms;
 
+import java.util.Arrays;
+
 import ordenacao.Sorter;
 
 public class MergeSort implements Sorter {
 
     @Override
     public int[] sort(int[] elements) {
-        int [] sorted = elements.clone();
-        if(sorted.length <= 1){
+        int[] sorted = elements.clone();
+        if (sorted.length < 2) {
             return sorted;
         }
-        int metade = sorted.length/2;
-        int[] direita = new int[metade];
-        int[] esquerda = new int[sorted.length - metade];
-        for(int i = 0; i < metade; i++){
-            esquerda[i] = sorted[i];
-        }
-        for(int i = metade; i < sorted.length; i++){
-            direita[i-metade] = sorted[i];
-        }
+        int metade = (int) Math.floor(sorted.length / 2);
+        int[] esquerda = Arrays.copyOfRange(sorted, 0, metade);
+        int[] direita = Arrays.copyOfRange(sorted, metade ,sorted.length);
         esquerda = sort(esquerda);
         direita = sort(direita);
-        int[] listaOrdenada = juntar(esquerda, direita);
-        return listaOrdenada;
+        juntar(sorted, esquerda, direita);
+        return sorted;
     }
-
-    public int[] juntar(int[] esquerda, int[] direita){
+    
+    public void juntar(int[] lista , int[] esquerda, int[] direita) {
         int i = 0;
         int j = 0;
-        int r = 0;
-        int[] lista = new int[esquerda.length + direita.length];
-        while(i < esquerda.length && j < direita.length){
-            if(esquerda[i] < direita[j]){
-                lista[r++] = esquerda[i++];
+        int r = 0;        
+        while (i < esquerda.length && j < direita.length) {
+            if (esquerda[i] <= direita[j]) {
+                lista[r] = esquerda[i];
+                i++;
+            } else {
+                lista[r] = direita[j];
+                j++;
             }
-            else{
-                lista[r++] = direita[j++];
-            }
+         r++;            
         }
-        while(i < esquerda.length){
-            lista[r++] = esquerda[i++];
+        while (i < esquerda.length) {
+            lista[r] = esquerda[i];
+            i++;
+            r++;
         }
-        while(j < direita.length){
-            lista[r++] = direita[j++];
-        }
-        return lista;
+        while (j < direita.length) {
+            lista[r] = direita[j];
+            j++;
+            r++;
+        }        
     }
+    
 
     @Override
     public String getName() {
